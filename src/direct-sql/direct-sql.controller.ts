39a7d67d6220a512f3b0d39d8391f2086d88a230
Milos Spasojevic <milos.s.pfc@gmail.com>
2020-06-30 16:35:46 +0200

    Update direct sql samples to clarify

--- src/direct-sql/direct-sql.controller.ts
+++ src/direct-sql/direct-sql.controller.ts
@@ -5,20 +5,20 @@ import { SqlService, Parameter } from "../sql/sql.service";
 export class DirectSqlController {
   constructor(private sql: SqlService) {}
 
-  @Get() // GET apiURL/direct-sql?acName=name
-  async GetQueryResults(@Query("acName") acName: string) {
+  @Get() // GET apiURL/direct-sql?name=some-name
+  async GetQueryResults(@Query("name") name: string) {
     const res = await this.sql.executeQuery(
       `select top 1 * from tHE_SetItem where acName = @acName`,
-      [Parameter("acName", acName)]
+      [Parameter("acName", name)]
     );
     return JSON.stringify(res);
   }
 
-  @Get("procedure") // GET apiURL/direct-sql/procedure?anQty=1
-  async GetStoredProcedureResults(@Query("anQty") anQty: number) {
+  @Get("procedure") // GET apiURL/direct-sql/procedure?quantity=1
+  async GetStoredProcedureResults(@Query("quantity") quantity: number) {
     const res = await this.sql.executeProcedure(
       "_erp_pProcedureThatTakesanQty",
-      [Parameter("anQty", anQty)]
+      [Parameter("anQty", quantity)] // Parameter takes a name and a value, and turns it into a parameter for the query or function
     );
     return JSON.stringify(res);
   }
